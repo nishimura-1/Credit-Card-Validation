1b64017c94a16751644fae60d23fe7e49bea28e6
nishimura <megan.ouchida@gmail.com>
2022-02-19 11:57:50 -0800

    first commit

--- main.js
+++ main.js
@@ -22,25 +22,98 @@ const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3]
 // An array of all the arrays above
 const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5]
 
-
 // Add your functions below:
 
-const validateCred(array) => {
-    const validation = [];
-    for (let i=array.length-1; i > 0; i--) {
-        
 
-
-        2 * (array.filter((element, index) => {
-            return index % 2 === 0;
-        }))
-        validation++
+//iterate to the left
+function validateCred(array) {
+    let luhnArray = [];
+    for (let i=array.length-1; i>=0; i--) {
+        //add even indexes to luhnArray
+        if ((array.length - 1 - i) % 2 === 0) {
+          luhnArray.push(array[i]);
+        }
+        else {
+          let double = array[i] * 2;
+          if (double > 9) {
+              double -= 9;
+            }
+            luhnArray.push(double);
+        }
     }
-    return validation;
+    //sum up all the digits in the credit card
+    let sumModulo = luhnArray.reduce(sum, 0);
+    function sum(accumulator, i) {
+        return accumulator + i;
+    }
+    //console.log("luhnArray: " + luhnArray);
+    //console.log("sumModulo: " + sumModulo);
+    //validity check
+    if (sumModulo % 10 === 0) {
+        return true;
+    }
+    return false;
 }
 
+//console.log(validateCred(mystery1));
 
 
+function findInvalidCards(cards) {
+  let invalid = cards.filter(card => {
+    return validateCred(card) === false;
+  })
+  return invalid;
+}
+
+//console.log(findInvalidCards(batch));
 
 
+function findInvalidCards(cards) {
+  const invalid = [];
+  for (let i=0; i < cards.length; i++) {
+    let card = cards[i];
+    if (validateCred(card) === false) {
+      invalid.push(card);
+    }
+  }
+    return invalid;
+  }
 
+//console.log(findInvalidCards(batch));
+
+
+function idInvalidCardCompanies(invalidCards) {
+  const companies = [];
+  for (let i = 0; i < invalidCards.length; i++) {
+    switch (invalidCards[i][0]) {
+      case 3: 
+        if (companies.indexOf('Amex') === -1) {
+          companies.push('Amex');
+        }
+          break
+      case 4: 
+        if (companies.indexOf('Visa') === -1) {
+          companies.push('Visa');
+      }
+          break
+      case 5:
+        if (companies.indexOf('Mastercard') === -1) {
+          companies.push('Mastercard');
+      }
+          break
+      case 6:
+        if (companies.indexOf('Discover') === -1) {
+          companies.push('Discover');
+        }
+            break
+      default:
+        console.log('Company Not Found');
+    }
+  }
+  return companies;
+}
+
+//console.log(idInvalidCardCompanies([invalid1])); // Should print['visa']
+//console.log(idInvalidCardCompanies([invalid3])); // Should print['Amex']
+//console.log(idInvalidCardCompanies([invalid2])); // Should print ['mastercard']
+console.log(idInvalidCardCompanies(batch)); // Find out which companies have mailed out invalid cards
